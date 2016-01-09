1f59844028138b5422a62989fa1092e28fced207
ProfessorX20 <ssbiljan@gmail.com>
2016-01-09 22:56:48 +0100

    Forgot the /help stuff

--- UserSpecificFunctions/USFCommands.cs
+++ UserSpecificFunctions/USFCommands.cs
@@ -14,7 +14,64 @@ namespace UserSpecificFunctions
     {
         public static void Help(CommandArgs args)
         {
+            if (args.Parameters.Count > 1)
+            {
+                args.Player.SendErrorMessage("Invalid syntax! Proper syntax: {0}help <command/page>", TShock.Config.CommandSpecifier);
+                return;
+            }
 
+            int pageNumber;
+            if (args.Parameters.Count == 0 || int.TryParse(args.Parameters[0], out pageNumber))
+            {
+                if (!PaginationTools.TryParsePageNumber(args.Parameters, 0, args.Player, out pageNumber))
+                {
+                    return;
+                }
+
+                IEnumerable<string> cmdNames = from cmd in Commands.ChatCommands
+                                               where cmd.CanRun(args.Player)
+                                               || (Utils.CheckPerm(args.Player.User.ID, cmd.Permissions[0])) && (cmd.Name != "auth" || TShock.AuthToken != 0)
+                                               orderby cmd.Name
+                                               select TShock.Config.CommandSpecifier + cmd.Name;
+
+                PaginationTools.SendPage(args.Player, pageNumber, PaginationTools.BuildLinesFromTerms(cmdNames),
+                    new PaginationTools.Settings
+                    {
+                        HeaderFormat = "Commands ({0}/{1}):",
+                        FooterFormat = "Type {0}help {{0}} for more.".SFormat(TShock.Config.CommandSpecifier)
+                    });
+            }
+            else
+            {
+                string commandName = args.Parameters[0].ToLower();
+                if (commandName.StartsWith(TShock.Config.CommandSpecifier))
+                {
+                    commandName = commandName.Substring(1);
+                }
+
+                Command command = Commands.ChatCommands.Find(c => c.Names.Contains(commandName));
+                if (command == null)
+                {
+                    args.Player.SendErrorMessage("Invalid command.");
+                    return;
+                }
+                if (!command.CanRun(args.Player) && !Utils.CheckPerm(args.Player.User.ID, command.Permissions[0]))
+                {
+                    args.Player.SendErrorMessage("You do not have access to this command.");
+                    return;
+                }
+
+                args.Player.SendSuccessMessage("{0}{1} help: ", TShock.Config.CommandSpecifier, command.Name);
+                if (command.HelpDesc == null)
+                {
+                    args.Player.SendInfoMessage(command.HelpText);
+                    return;
+                }
+                foreach (string line in command.HelpDesc)
+                {
+                    args.Player.SendInfoMessage(line);
+                }
+            }
         }
 
         public static void USFMain(CommandArgs args)
